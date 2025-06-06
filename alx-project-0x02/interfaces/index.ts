739d31c003a06c25b4dc78948340c85c7cfa566c
ahmed <ahmedabdelnaser3010@gmail.com>
2025-06-07 02:55:49 +0300

    task three

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,4 +1,6 @@
 
-export interface T {
+export interface CardProps {
+    title: string;
+    content: string
 
 }
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,9 +1,14 @@
+import Card from '@/components/common/Card'
 import React from 'react'
 
 const home = () => {
     return (
         <h1>
-            home page
+            <Card title='project one' content='hello project one' />
+            <Card title='project two' content='hello project two' />
+            <Card title='project three' content='hello project three' />
+            <Card title='project four' content='hello project four' />
+            <Card title='project five' content='hello project five' />
         </h1>
     )
 }
